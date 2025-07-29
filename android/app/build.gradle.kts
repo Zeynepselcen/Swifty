daf2039d37d2509893c7d258e4a58e0431a892af
selcen <82097198+Zeynepselcen@users.noreply.github.com>
2025-07-30 00:41:20 +0300

    v1.2.2+9: Versiyon numaraları güncellendi ve AAB oluşturuldu

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.swifty.gallerycleaner"
         minSdk = 21
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.1.5"
+        versionCode = 9
+        versionName = "1.2.2"
     }
 
     signingConfigs {
